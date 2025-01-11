d2cf785b3b3233cb3b5b51d180a54fbc6dc6b1fb
Mehmet Emin Çakın <mehmetemincakin@gmail.com>
2025-01-12 01:50:32 +0300

    removed unused import

--- lib/src/features/authentication/screens/signup/register_screen.dart
+++ lib/src/features/authentication/screens/signup/register_screen.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-import 'package:yol_al/src/features/authentication/controllers/signup_controller.dart';
 import 'package:yol_al/src/features/authentication/screens/signup/widgets/register_form_widget.dart';
 
 class RegisterScreen extends StatelessWidget {
